ac89d5c754e4e8651489138226870bfc30ec7643
dimikere <dimikere@gmail.com>
2019-03-05 22:09:47 +0200

    Added the get method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Post::get(),200);
+    }
 }
